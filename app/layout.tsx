6e7bccaaf6d4f3a3894c66022cdaa61e0359a828
Christine Xaysakda <104770147+K3ikei@users.noreply.github.com>
2026-06-02 11:23:35 +0200

    suppressHydrationWarning avoid impact from Grammarly browser extension installed in Chrome

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       className={poppins.className}
       suppressHydrationWarning
     >
-      <body className="flex flex-col min-h-screen">
+      <body className="flex flex-col min-h-screen" suppressHydrationWarning>
         <RootProvider
           theme={{
             enabled: false,
